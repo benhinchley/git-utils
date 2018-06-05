e73968d5459cc40b070eeead55120f3840702f4c
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-06-06 09:56:29 +1000

    rm-dir: add some docs

---
+++ rm-dir/README.md
@@ -0,0 +1,9 @@
+# rm-dir
+> rm-dir removes directories from a git repository's history
+
+## Usage
+```bash
+Usage of rm-dir:
+  -dirs string
+        comma seperated list of directories to remove
+```
--- rm-dir/main.go
+++ rm-dir/main.go
@@ -18,7 +18,13 @@ func main() {
 	flag.StringVar(&directories, "dirs", "", "comma seperated list of directories to remove")
 	flag.Parse()
 
-	for _, dir := range strings.Split(directories, ",") {
+	dirs := strings.Split(directories, ",")
+	if len(dirs) == 1 && dirs[0] == "" {
+		fmt.Println("at least 1 directory is required")
+		os.Exit(1)
+	}
+
+	for _, dir := range dirs {
 		args := []string{"filter-branch", "-f", "--index-filter", fmt.Sprintf("\"git rm --cached --ignore-unmatch -r %s\"", dir), "--prune-empty"}
 		cmd := exec.Command("git", args...)
 
