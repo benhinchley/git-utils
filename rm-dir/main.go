a151aac00f3e62698cc307f684726d51f214144e
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-06-06 09:34:35 +1000

    rm-dir: allow for multiple directories to be removed

--- rm-dir/main.go
+++ rm-dir/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
+)
+
+var (
+	directories string
 )
 
 // git filter-branch -f --index-filter "git rm --cached --ignore-unmatch -r <dir>" --prune-empty
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("a directory to remove is required")
-		os.Exit(1)
-	}
+	flag.StringVar(&directories, "dirs", "", "comma seperated list of directories to remove")
+	flag.Parse()
 
-	dir := os.Args[1]
-	args := []string{"filter-branch", "-f", "--index-filter", fmt.Sprintf("\"git rm --cached --ignore-unmatch -r %s\"", dir), "--prune-empty"}
-	cmd := exec.Command("git", args...)
+	for _, dir := range strings.Split(directories, ",") {
+		args := []string{"filter-branch", "-f", "--index-filter", fmt.Sprintf("\"git rm --cached --ignore-unmatch -r %s\"", dir), "--prune-empty"}
+		cmd := exec.Command("git", args...)
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
